4f57dcbb9816653108690dc3e7cb1e81b63ce5be
RungFif <rafifpranaja301@gmail.com>
2024-09-10 08:23:21 +0700

    Personal Portfolio

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle navigation menu for mobile
+document.getElementById("menu-toggle").addEventListener("click", function () {
+  const nav = document.querySelector("nav ul");
+  nav.classList.toggle("show");
+});
